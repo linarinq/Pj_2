ad0c42637b11f11aad0bf890dfad19b8e9c96372
linarinq <vera.s88@mail.ru>
2024-09-14 17:29:47 +0300

    camera and player scripts

---
+++ Project/Assets/Camera.cs
@@ -0,0 +1,48 @@
+
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+  [SerializeField] GameObject player;
+	[SerializeField] [Range(0.5f, 2f)]
+	float mouseSense = 1;
+	[SerializeField] [Range(15, 25)]
+	int lookUp = -15;
+	[SerializeField] [Range(15, 25)]
+	int lookDown = 20;
+
+
+
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update ()
+	{
+    if(isSpectrator == false)
+    {
+      //rotating
+    }
+    else
+    {
+      float rotateX = Input.GetAxis("Mouse X") * mouseSense;
+      float rotateY = Input.GetAxis("Mouse Y") * mouseSense;
+
+      Vector3 rotCamera = transform.rotation.eulerAngles;
+      Vector3 rotPlayer = player.transform.rotation.eulerAngles;
+
+      rotCamera.x = (rotCamera.x > 180) ? rotCamera.x - 360 : rotCamera.x;
+      rotCamera.x = Mathf.Clamp(rotCamera.x, lookUp, lookDown);
+      rotCamera.x -= rotateY;
+
+      rotCamera.z = 0;
+      rotPlayer.y += rotateX;
+
+      transform.rotation = Quaternion.Euler(rotCamera);
+      player.transform.rotation = Quaternion.Euler(rotPlayer);
+    }
+
+	}
+}
--- Project/Assets/PlayerController.cs
+++ Project/Assets/PlayerController.cs
@@ -10,6 +10,13 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     Vector3 direction;
 
+    [SerializeField] float shiftSpeed = 10f;
+  	[SerializeField] float jumpForce = 7f;
+  	bool isGrounded = true;
+
+    // Audio
+  	[SerializeField] AudioSource characterSounds;	[SerializeField] AudioClip jump;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +31,31 @@ public class PlayerController : MonoBehaviour
       float moveVertical = Input.GetAxis("Vertical");
       direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
       direction = transform.TransformDirection(direction);
+
+      if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+      {
+        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        isGrounded = false;
+      }
+
+      if(Input.GetKeyDown(KeyCode.LeftShift))
+      {
+        currentSpeed = shiftSpeed;
+      }
+
+      if(Input.GetKeyUp(KeyCode.LeftShift))
+      {
+        currentSpeed = movementSpeed;
+      }
     }
 
     void FixedUpdate()
     {
       rb.MovePosition(transform.position + direction * currentSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+      isGrounded = true;
     }
 }
